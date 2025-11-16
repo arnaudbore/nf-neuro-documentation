b10bf4484c607cc2c341cda1482a694152e1dc6e
Alex Valcourt Caron <alexvc91@gmail.com>
2025-11-16 02:35:24 +0000

    implement scroller in hero page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,14 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	pipelines: defineCollection({
+		type: 'data',
+		schema: z.object({
+			name: z.string(),
+			organisation: z.string(),
+		}),
+	}),
 };
